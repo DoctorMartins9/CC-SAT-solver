0941eccd04d1886aaf5b453e8f6755c6b2f35bf7
Enrico Martini <enrico.martini_02@studenti.univr.it>
2020-01-15 21:51:32 +0100

    Added r7 benchmark

--- build/CMakeFiles/cyclo.dir/DependInfo.cmake
+++ build/CMakeFiles/cyclo.dir/DependInfo.cmake
@@ -1,17 +1,7 @@
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "CXX"
   )
 # The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/parco04/CC-SAT-solver/tests/test.cpp" "/home/parco04/CC-SAT-solver/build/CMakeFiles/test.dir/tests/test.cpp.o"
-  )
-set(CMAKE_CXX_COMPILER_ID "GNU")
-
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "../include"
-  )
 
 # Targets to which this target links.
 set(CMAKE_TARGET_LINKED_INFO_FILES
---
+++ build/CMakeFiles/validation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/validation.dir/tests/test.cpp.o"
+  "validation.pdb"
+  "validation"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/validation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
